9aa755d4548c3b680e84e62923cf35dc2b43d47c
dominikpalatynski <palatynski.dominik@gmail.com>
2023-03-21 22:18:25 +0100

    stories modal

--- src/app/recipe-modal/recipe-modal-detail/recipe-modal-detail.component.ts
+++ src/app/recipe-modal/recipe-modal-detail/recipe-modal-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Params, Router } from '@angular/router';
+import { RecipeService } from 'src/app/shared/recipe.service';
+import { Recipe } from 'src/app/shared/recipe-model';
+import { User } from 'src/app/shared/auh-model';
+import { AuthService } from 'src/app/shared/auth.service';
 @Component({
   selector: 'app-recipe-modal-detail',
   templateUrl: './recipe-modal-detail.component.html',
-  styleUrls: ['./recipe-modal-detail.component.scss']
+  styleUrls: ['./recipe-modal-detail.component.scss'],
 })
-export class RecipeModalDetailComponent {
+export class RecipeModalDetailComponent implements OnInit {
+  constructor(
+    private recipeService: RecipeService,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  currentUser!: User;
+  modalRecipe!: Recipe;
+  recipeId!: number;
 
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.recipeId = params['id'];
+    });
+    this.modalRecipe = this.recipeService.exportToModel(this.recipeId);
+    console.log(this.modalRecipe);
+    this.authService.currentUserChanged.subscribe(
+      (user) => (this.currentUser = user)
+    );
+  }
 }
